cb6107a31e97a22547879be19df8c8720366c295
Joel Boersma <joelboersma@gmail.com>
2019-07-06 11:01:42 -0700

    Marking emails as read/unread works again

--- src/app/email-list/email-list.component.ts
+++ src/app/email-list/email-list.component.ts
@@ -6,50 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-list.component.scss']
 })
 export class EmailListComponent implements OnInit {
+  public styleRead = {
+    color: '#00000000'
+  }
+  public styleUnread = {
+    color: '#3579c7'
+  }
+
   public emails = [
     {
       subject: "Childcare",
       sender: "Peter Fisher",
       date: "8:26 am",
       message: "Hello, I'm wondering if you would be able to watch my child on Friday evening. My wife and I are going to see Shrek 56 in theaters. We've been waiting for this for years. Let me know if you can. Thanks!",
-      listPos: 0
+      listPos: 0,
+      style: this.styleUnread
     },
     {
       subject: "Latin Jibberish Lessons",
       sender: "Caeser",
       date: "June 28",
       message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore recusandae distinctio ipsam dolores ut temporibus reprehenderit quos sit! Consectetur asperiores doloremque ipsum temporibus a quo ut vitae veritatis aliquam ex.",
-      listPos: 1
+      listPos: 1,
+      style: this.styleUnread
     },
     {
       subject: "Help Me",
       sender: "Nigerian Prince",
       date: "April 20",
       message: "paypal me 30000 usd",
-      listPos: 2
+      listPos: 2,
+      style: this.styleUnread
     },
     {
       subject: "Super Smash Bros. Redux",
       sender: "Masahiro Sakurai",
       date: "December 7",
       message: "The next Super Smash Bros. game will have Goku. I've listened to fan feedback for years and I've been neglecting this particular character for so long. I wanted to have some bit of rules for who gets in. But this has been going on for too long. And I've finally accepted that literally anyone can get in. I hope you look forward to playing as Goku in 20XX.",
-      listPos: 3
+      listPos: 3,
+      style: this.styleUnread
     }
-  ];
-
-  private styleRead = {
-    color: '#00000000'
-  }
-  private styleUnread = {
-    color: '#3579c7'
-  }
-
-  public startingStyle = this.styleUnread;
-  public styles = [
-    this.startingStyle, 
-    this.startingStyle, 
-    this.startingStyle, 
-    this.startingStyle
   ];
 
   constructor() { }
@@ -59,11 +55,11 @@ export class EmailListComponent implements OnInit {
   }
 
   markRead(pos: number) {
-    this.styles[pos] = this.styleRead;
+    this.emails[pos].style = this.styleRead;
   }
 
   markUnread(pos: number) {
-    this.styles[pos] = this.styleUnread;
+    this.emails[pos].style = this.styleUnread;
   }
 
   toggleRead(event) {

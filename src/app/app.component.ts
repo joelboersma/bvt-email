7940d41d5d6a24549efa1e67cdd380288ae3710c
Joel Boersma <joelboersma@gmail.com>
2019-07-08 14:34:26 -0700

    Sidebar adjustment based on screen size  takes a lot less work now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,5 @@ export class AppComponent {
   title = 'week-one-project';
   public windowWidth: any;
 
-  ngOnInit() {
-    this.windowWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  whenResize(event) {
-    this.windowWidth = window.innerWidth;
-  }
-
-  useBigView(): boolean {
-    return this.windowWidth >= 768;
-  }
+  ngOnInit() {}
 }
